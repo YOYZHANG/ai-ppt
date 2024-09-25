1ddca253c34a3ba48c556e02fd317623c853f328
yoyzhang <alexuzhang@gmail.com>
2024-09-25 12:06:17 +0800

    fix: chore

---
+++ app/api/convertd/route.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function POST(req) {
+  const { artifact} = await req.json()
+
+    const fileName = `ppt_${Date.now()}.html`;
+    const filePath = path.join(process.cwd(),'public', 'presentations', fileName);
+    await fs.writeFile(filePath, artifact.code, () => {});
+
+
+    // Send file URL back to client
+    return new Response(JSON.stringify({
+      url: `presentations/${fileName}`
+    }))
+}
